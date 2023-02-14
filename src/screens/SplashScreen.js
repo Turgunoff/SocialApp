26addb4052f26b4cae41439a283b3f245cbf27fd
Eldor <eldorturgunov77@gmail.com>
2023-02-14 18:33:13 +0500

    .

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,13 +1,32 @@
-import { Button } from "native-base"
-import { StyleSheet, View } from "react-native"
-import { NativeBaseProvider, Text } from "native-base"
-const SplashScreen = ({ navigation }) => {
+import { StyleSheet, Image } from "react-native"
+import { NativeBaseProvider } from "native-base"
+import Onboarding from "react-native-onboarding-swiper"
+
+const SplashScreen = () => {
   return (
     <NativeBaseProvider>
-      <View>
-        <Text>SplashScreen</Text>
-        <Button title='click' onPress={() => navigation.navigate("Login")} />
-      </View>
+      <Onboarding
+        pages={[
+          {
+            backgroundColor: "#464646",
+            image: <Image source={require("../../assets/splash.png")} />,
+            title: "Onboarding",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+          {
+            backgroundColor: "#121212",
+            image: <Image source={require("../../assets/splash.png")} />,
+            title: "Onboarding 2",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+          {
+            backgroundColor: "#828282",
+            image: <Image source={require("../../assets/splash.png")} />,
+            title: "Onboarding 3",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+        ]}
+      />
     </NativeBaseProvider>
   )
 }
